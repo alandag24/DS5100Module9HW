74f9383a7b2a466412adca1a12527b397ab99b44
alandag24 <alandag24@gmail.com>
2024-07-02 15:36:19 -0400

    created demo file

---
+++ book_lover_demo.py
@@ -0,0 +1,7 @@
+from booklover import booklover
+
+lover = booklover.BookLover("Alanda","alandag24@gmail.com","mystery")
+
+lover.add_book("Harry Potter", 4)
+
+lover.num_books_read
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name ='Test',
+    version ='0.1',
+    description='Booklover package for hw9',
+    url='https://github.com/alandag24/DS5100Module9HW',
+    author='Alanda Guan',
+    author_email='alandag24@gmail.com',
+    license='MIT',
+    packages=['booklover']
+
+
+)
